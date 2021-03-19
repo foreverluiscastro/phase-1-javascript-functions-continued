54e9aaa13fa8b68b556e071b7180cc59fee34fe0
luis <foreverluiscastro@gmail.com>
2021-03-18 23:29:40 -0400

    commit tree aec23c96aebaa7b5050d213ff5c42fcc3c8b98da

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+const mondayWork = function(activity= "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(sym) {
+    if((sym == "*") || (sym =="||")) {
+        return (adjective = "special") => {
+            return `You are ${sym}${adjective}${sym}!`
+        }
+    }
+    
+}
